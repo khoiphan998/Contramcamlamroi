c2eda46fc255a0cbfeb909c561c414f62090995a
Phan Trần Anh Khôi <21dh113276@st.huflit.edu.vn>
2022-11-19 01:03:53 +0700

    update 1am 19/11/2022

--- Contramcamlamroi/Scripts/script.js
+++ Contramcamlamroi/Scripts/script.js
@@ -16,3 +16,12 @@ $('.navTrigger').click(function () {
 
 });
 
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById(".navbar-inverse").style.top = "0";
+    } else {
+        document.getElementById(".navbar-inverse").style.top = "-100%";
+    }
+}
